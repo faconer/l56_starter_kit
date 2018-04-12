56bc3ffc7c845b2fc7b4435987bb6e5dff4c2b42
Nguyen Trung Thanh <thanhcttsp@gmail.com>
2018-04-12 16:22:40 +0700

    Animation flash message

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -3,6 +3,7 @@
 <head>
     <title>Admin</title>
     <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+    @yield('header')
 </head>
 <body>
     @include('flash::message')
@@ -14,5 +15,11 @@
         </ul>
     @endif
     @yield('content')
+    <script src="//code.jquery.com/jquery-3.3.1.min.js"></script>
+    <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+    <script>
+        $('div.alert').not('.alert-important').delay(3000).fadeOut(350);
+    </script>
+    @yield('scripts')
 </body>
 </html>
